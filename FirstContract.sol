8e08d9f07dc296f1a3d8e54bd114e495ce5a03a7
alvdelci <delcimarioalvesdev@gmail.com>
2025-06-12 18:22:35 -0300

    initial commit

---
+++ FirstContract.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.7;
+
+contract FirstContract {
+    uint256 public numero = 10; // variavel de estado, permanente na blockchain (até que seja alterado)
+    string public texto = 'qualquer texto';
+}
