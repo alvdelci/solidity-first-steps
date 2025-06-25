0dd8c16f9155d6d5cd5635bba03557c1a70e2b14
alvdelci <delcimarioalvesdev@gmail.com>
2025-06-25 19:17:34 -0300

    criando funcao para acessar valor de variavel private e internal

---
+++ GetContract.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+contract GetVar {
+    int numero = 10; //Se nao declarar a visibilidade ela sera INTERNAL
+
+    //Se a variavel nao for declarada como PUBLIC seu valor so podera ser acessado de fora do contrato atraves de uma funcao
+    function getNumero() public view returns(int) {
+        return numero;
+    }
+
+}
